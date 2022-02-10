fbb583485dbfcbace4a73d15027d310bb957526c
KimiKimar27 <blazerod65@gmail.com>
2022-02-10 21:48:45 +0100

    Added win/loss streaks

--- data.py
+++ data.py
@@ -7,6 +7,8 @@ line 4: fourth row wins
 line 5: fifth row wins
 line 6: total wins
 line 7: total losses
+line 8: streak type - loss (0) or win (1)
+line 9: streak count
 """
 
 from termcolor import colored
@@ -17,7 +19,7 @@ def reset_data():
   print("This may have been caused by tampering with the data file or the data file hasn't been created yet")
   with open("data", "r+") as data_file:
     data_file.truncate(0)
-    data_file.write("0\n0\n0\n0\n0\n0\n0")
+    data_file.write("0\n0\n0\n0\n0\n0\n0\n0\n0")
 
 def check_corruption():
   wins_calculated = 0
@@ -31,11 +33,16 @@ def check_corruption():
     # File contents must be strictly numbers
     try: lines = [int(i) for i in data_file.readlines()]
     except ValueError: return True
-    # File must contain exactly 7 lines
-    if len(lines) != 7: return True
+    # File must contain exactly 9 lines
+    if len(lines) != 9: return True
     # Sum of wins by row must be equal to total wins
     for i in range(5): wins_calculated += lines[i]
     if wins_calculated != lines[5]: return True
+    # Streak type must be strictly a 1 or a 0
+    if lines[7] not in (0, 1): return True
+    # Streak count can't be larger than win/loss totals
+    if lines[7] == 0 and lines[6] < lines[8]: return True
+    if lines[7] == 1 and lines[5] < lines[8]: return True
 
   return False
 
@@ -43,13 +50,21 @@ def update_data(game_won, attempt_no):
   with open("data", "r+") as data_file:
     lines = [int(i) for i in data_file.readlines()]
     data_file.seek(0)
+    # If streak type has changed, set streak count to 0, if not, increment streak count by 1
+    if (lines[7] == 0 and game_won == True) or (lines[7] == 1 and game_won == False): lines[8] = 1
+    else: lines[8] += 1
+    # Set streak type
+    if game_won == True: lines[7] = 1
+    else: lines[7] = 0
+    # Update total wins/losses
     if game_won:
       lines[attempt_no - 1] += 1
       lines[5] += 1
     else:
       lines[6] += 1
-    for i in range(7):
-      if i != 6: data_file.write(f"{lines[i]}\n")
+    # Write new data to file
+    for i in range(9):
+      if i != 8: data_file.write(f"{lines[i]}\n")
       else: data_file.write(f"{lines[i]}")
 
 def print_data():
@@ -59,6 +74,8 @@ def print_data():
   for i in range(5):
     print(f"Wins by row {i + 1}: {lines[i]}")
   print("\nWins/Losses: {}/{}, Win%: {:.1%}".format(wins, losses, wins / (wins + losses)))
+  if lines[7] == 1: print(f"You are currently on a {lines[8]} game winning streak")
+  else: print(f"You are currently on a {lines[8]} game losing streak")
 
 def access_data(game_won, attempt_no):
   if check_corruption():
